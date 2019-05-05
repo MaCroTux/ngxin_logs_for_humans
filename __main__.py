b2e7b571d6c1f24a49a968ee8da0f042e94140fe
MaCroTux <macrotux@gmail.com>
2019-05-05 13:39:30 +0200

    WIP

--- __main__.py
+++ __main__.py
@@ -1,53 +1,7 @@
 import sys
 import re
 import json
-
-class bcolors:
-
-    CEND = '\33[0m'
-    CBOLD = '\33[1m'
-    CITALIC = '\33[3m'
-    CURL = '\33[4m'
-    CBLINK = '\33[5m'
-    CBLINK2 = '\33[6m'
-    CSELECTED = '\33[7m'
-
-    CBLACK = '\33[30m'
-    CRED = '\33[31m'
-    CGREEN = '\33[32m'
-    CYELLOW = '\33[33m'
-    CBLUE = '\33[34m'
-    CVIOLET = '\33[35m'
-    CBEIGE = '\33[36m'
-    CWHITE = '\33[37m'
-
-    CBLACKBG = '\33[40m'
-    CREDBG = '\33[41m'
-    CGREENBG = '\33[42m'
-    CYELLOWBG = '\33[43m'
-    CBLUEBG = '\33[44m'
-    CVIOLETBG = '\33[45m'
-    CBEIGEBG = '\33[46m'
-    CWHITEBG = '\33[47m'
-
-    CGREY = '\33[90m'
-    CRED2 = '\33[91m'
-    CGREEN2 = '\33[92m'
-    CYELLOW2 = '\33[93m'
-    CBLUE2 = '\33[94m'
-    CVIOLET2 = '\33[95m'
-    CBEIGE2 = '\33[96m'
-    CWHITE2 = '\33[97m'
-
-    CGREYBG = '\33[100m'
-    CREDBG2 = '\33[101m'
-    CGREENBG2 = '\33[102m'
-    CYELLOWBG2 = '\33[103m'
-    CBLUEBG2 = '\33[104m'
-    CVIOLETBG2 = '\33[105m'
-    CBEIGEBG2 = '\33[106m'
-    CWHITEBG2 = '\33[107m'
-
+from bcolors import Bcolors
 
 class Data:
     IP = 'ip'
@@ -69,7 +23,7 @@ def read_line():
         line_read = sys.stdin.readline()
 
         if not line_read:
-            print('Flujo de datos cortado')
+            print('End revice data')
             return False
 
         return line_read
@@ -121,19 +75,19 @@ while True:
     if not line:
         break
 
-    color = bcolors.CEND
+    color = Bcolors.CEND
 
     if group[Data.STATUS] == 500:
         error_500 += 1
-        color = bcolors.CREDBG
+        color = Bcolors.CREDBG
 
     if group[Data.STATUS] == 404:
         error_404 += 1
-        color = bcolors.CYELLOW2
+        color = Bcolors.CYELLOW2
 
     if group[Data.STATUS] == 429:
         error_404 += 1
-        color = bcolors.CBLUE
+        color = Bcolors.CBLUE
 
     print(color + (
         " {0:4d} {1:16s} {2:16s} {3:60s} {4:6s} {5:6s} {6:9s}".format(
@@ -144,5 +98,5 @@ while True:
             str(error_500) + ' [500]',
             str(error_404) + ' [404]',
             group[Data.REFERER][0:120]
-        )) + bcolors.CEND
+        )) + Bcolors.CEND
     )
